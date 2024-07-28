7bbc863734982463ff83921ab87d732dc6c7ea8f
Sergey Kardapoltsev <kardapoltsev@list.ru>
2024-07-28 16:11:08 +0300

    новые файлы

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'test_deploy'
+project = 'test-deploy'
 copyright = '2024, SergeyKardapoltsev'
 author = 'SergeyKardapoltsev'
 
